011cd4ec660011cb123104d9105d1b6c0779eb13
José Carlos <j.de.oliveira.lima@avanade.com>
2019-08-09 17:31:27 -0300

    input para tags

--- src/app/components/dialog/add-tool/add-tool.component.ts
+++ src/app/components/dialog/add-tool/add-tool.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialogRef, MAT_DIALOG_DATA, MatChipInputEvent } from '@angular/material';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
 
 @Component({
   selector: 'app-add-tool',
@@ -8,12 +9,42 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 })
 export class AddToolComponent implements OnInit {
 
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  tags: string[] = [];
+
   constructor(
     public dialogRef: MatDialogRef<AddToolComponent>,
     @Inject(MAT_DIALOG_DATA) public data: string
   ) { }
 
   ngOnInit() {
+  }
+
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our fruit
+    if ((value || '').trim()) {
+      this.tags.push(value.trim());
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  remove(tag: string): void {
+    const index = this.tags.indexOf(tag);
+
+    if (index >= 0) {
+      this.tags.splice(index, 1);
+    }
   }
 
 }
